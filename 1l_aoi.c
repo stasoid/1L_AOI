cccf58a1688c00900acdab931cf56c27e0e53237
noname <noemail>
2017-12-21 23:04:40 +0600

    fix bug

--- 1l_aoi.c
+++ 1l_aoi.c
@@ -51,7 +51,7 @@ int main(int argc, char* argv[])
 	if(argc < 2) { puts(usage); return 1; }
 	if(argv[1][0] == '-')
 	{
-		if(!(argv[1][1] == 'e' && argv[1][2] == 'u')) { puts(usage); return 1; }
+		if(strcmp(argv[1], "-eu") != 0) { puts(usage); return 1; }
 		if(argc < 3) { puts(usage); return 1; }
 		eu = true;
 		filename = argv[2];
